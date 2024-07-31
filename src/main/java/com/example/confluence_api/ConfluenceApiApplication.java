9a4b0ee5a72917ea9a02efad8bc195f2cbecb95d
Javid-Sadigli <cavidoffical@gmail.com>
2024-07-31 14:12:19 +0400

    First commit

---
+++ src/main/java/com/example/confluence_api/ConfluenceApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.confluence_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfluenceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfluenceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/confluence_api/ConfluenceApiApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.confluence_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfluenceApiApplicationTests {
+
+	@Test
+	void contextLoads() 
+	{
+		
+	}
+
+}
